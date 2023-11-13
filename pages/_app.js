ceb06236b4b7293ff688db542acc834a25c1e3af
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2023-11-13 21:04:33 +0100

    testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
- import { ThemeProvider } from 'styled-components';
- import { theme } from '@/theme';
- import { client } from '@/sanity/lib/client'
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@/theme';
+import { client } from '@/sanity/lib/client';
 import Theme from '@/components/Theme/Theme';
+import Footer from '@/components/Footer/Footer';
 
-
- function MyApp({ Component, pageProps, theme }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <Theme> <Component {...pageProps} /></Theme>
-
+    <Theme>
+      <Component {...pageProps} />
+      <Footer />
+    </Theme>
   );
- }
+}
 
- export default MyApp;
+export default MyApp;
