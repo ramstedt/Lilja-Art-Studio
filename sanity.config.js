c88823275dbd02b36853f87d56b742bc60428be5
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2023-11-25 13:31:05 +0100

    Update sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -1,12 +1,6 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/pages/sanity/[[...index]].jsx` route
- */
-
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './sanity/env';
 import { schema } from './sanity/schema';
 import { simplerColorInput } from 'sanity-plugin-simpler-color-input';
@@ -16,17 +10,13 @@ export default defineConfig({
   basePath: '/sanity',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
     deskTool({
       structure: myStructure,
     }),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
     simplerColorInput({
-      // Note: These are both optional
       defaultEnableAlpha: true,
       defaultColorList: [
         { label: 'Background', value: '#fffffe' },
@@ -38,7 +28,7 @@ export default defineConfig({
         { label: 'Button Hover', value: '#fffffe' },
         { label: 'Button Text Hover', value: '#181818' },
         { label: 'Link', value: '#994ff3' },
-        { label: 'Link Hover', value: '#fbdd74' },
+        { label: 'Link Hover', value: '#f4c725' },
         { label: 'Custom...', value: 'custom' },
       ],
     }),
