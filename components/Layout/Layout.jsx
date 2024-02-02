70fbae1f533dfcc8571581db5cdb3065ca823160
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-02-03 00:20:31 +0100

    Update Layout.jsx

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -10,42 +10,17 @@ import { useState, useEffect } from 'react';
 import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder(client);
+
 function urlFor(source) {
   return builder.image(source);
 }
 
-function Layout({ children }) {
-  const [metadata, setMetadata] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    client
-      .fetch(`*[_type == "meta"]`)
-      .then((data) => {
-        setMetadata(data[0]);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []);
-  if (isLoading) return <div></div>;
-  return (
-    <Theme>
-      <Head>
-        <link rel='canonical' href='https://lilja-art-studio.vercel.app' />
-        <title>{metadata.title}</title>
-        <link rel='icon' href={urlFor(metadata.favicon).url()} />
-        <meta name='description' content={metadata.description} />
-        <meta name='keywords' content={metadata.keywords} />
-        <meta name='robots' content='index, follow' />
-      </Head>
 const GoogleTag = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.async = true;
     script.src = 'https://www.googletagmanager.com/gtag/js?id=G-Y5682L7L29';
-    document.head.appendChild(script);
+    document.head.insertBefore(script, document.head.firstChild);
 
     script.onload = () => {
       window.dataLayer = window.dataLayer || [];
@@ -64,8 +39,35 @@ const GoogleTag = () => {
   return null;
 };
 
-export default GoogleTag;
+function Layout({ children }) {
+  const [metadata, setMetadata] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
+  useEffect(() => {
+    client
+      .fetch(`*[_type == "meta"]`)
+      .then((data) => {
+        setMetadata(data[0]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  if (isLoading || !metadata) return <div></div>;
+
+  return (
+    <Theme>
+      <Head>
+        <GoogleTag />
+        <link rel='canonical' href='https://lilja-art-studio.vercel.app' />
+        <title>{metadata.title}</title>
+        <link rel='icon' href={urlFor(metadata.favicon).url()} />
+        <meta name='description' content={metadata.description} />
+        <meta name='keywords' content={metadata.keywords} />
+        <meta name='robots' content='index, follow' />
+      </Head>
       <GlobalStyle />
       <Hero />
       <Navbar />
