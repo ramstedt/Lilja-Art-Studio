3bb9c51eeb950df11fdb7dceacf95e612fb136d1
Emma Ramstedt <emma.ramstedt@gmail.com>
2024-10-23 18:54:21 +0200

    add video support to gallery

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,24 +1,22 @@
 // /pages/api/send-email.js
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { firstName, surname, email, phone, message, course } = req.body;
 
-    // Nodemailer transporter configuration
     let transporter = nodemailer.createTransport({
-      service: 'gmail', // Use your email service, e.g., Gmail
+      service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER, // Your email address
-        pass: process.env.EMAIL_PASS, // Your email password
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     try {
-      // Send the email
       await transporter.sendMail({
-        from: email, // Sender's email
-        to: process.env.EMAIL_TO, // Your email address (the recipient)
+        from: email,
+        to: process.env.EMAIL_TO,
         subject: `Bokning av kursplats: ${course}`,
         text: `
           Förnamn: ${firstName}
@@ -30,14 +28,12 @@ export default async function handler(req, res) {
           ${message}
         `,
       });
-
-      // Respond with success message
       res.status(200).json({ success: true });
     } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ error: 'Error sending email' });
+      console.error("Error sending email:", error);
+      res.status(500).json({ error: "Error sending email" });
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ message: "Method not allowed" });
   }
 }
